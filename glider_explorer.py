c03c82215d41b7896b9a11b41cd11d05a124ea50
MartinMohrmann <martin.mohrmann@voiceoftheocean.org>
2024-01-24 23:47:46 +0100

    working version, before cleaning up

--- glider_explorer.py
+++ glider_explorer.py
@@ -67,13 +67,13 @@ def create_single_ds_plot(data, metadata, variable, dsid, plt_props):
     endvline = hv.VLine(metadata.loc[dsid]['time_coverage_end (UTC)']).opts(color='grey')
     print(plt_props)
 
-    """
+
     raster = data.hvplot.scatter(
         x='time',
         y='depth',
         c=variable,
-        x_sampling=plt_props['x_sampling'],
-        y_sampling=plt_props['y_sampling'],
+        #x_sampling=plt_props['x_sampling'],
+        #y_sampling=plt_props['y_sampling'],
         flip_yaxis=True,
         dynamic=False,
         cmap=cmocean.cm.thermal,
@@ -85,7 +85,7 @@ def create_single_ds_plot(data, metadata, variable, dsid, plt_props):
 
         #clim=clim,
         )
-    """
+
     """
     means = dsh.mean(variable)
     points = hv.Points(data, ['time', 'depth'])
@@ -103,8 +103,60 @@ def create_single_ds_plot(data, metadata, variable, dsid, plt_props):
     """
     return text_annotation*startvline*endvline#*raster
 
+def create_single_ds_plot_raster(data, metadata, variable, plt_props):
+    # plot_options should be a dictionary
+    #import pdb; pdb.set_trace()
+    """
+    text_annotation = hv.Text(
+        x=metadata.loc[dsid]['time_coverage_start (UTC)'] ,
+        y=-2, text=dsid.replace('nrt_', ''),
+        fontsize=plt_props['dynfontsize'],
+            ).opts(text_opts
+            ).opts(**ropts)
 
-def get_xsection(x_range, variable):
+    startvline = hv.VLine(metadata.loc[dsid]['time_coverage_start (UTC)']).opts(color='grey')
+    endvline = hv.VLine(metadata.loc[dsid]['time_coverage_end (UTC)']).opts(color='grey')
+    print(plt_props)
+
+    """
+    raster = data.hvplot.scatter(
+        x='time',
+        y='depth',
+        c=variable,
+        #x_sampling=plt_props['x_sampling'],
+        #y_sampling=plt_props['y_sampling'],
+        #flip_yaxis=True,
+        #dynamic=False,
+        cmap=cmocean.cm.thermal,
+        #height=400,
+        #responsive=True,
+        #datashade=True,
+        #rasterize=True,
+        #cnorm=cnorm,*rasterize(
+
+        #clim=clim,
+        )
+
+    """
+    means = dsh.mean(variable)
+    points = hv.Points(data, ['time', 'depth'])
+    raster = rasterize(points,
+                  aggregator=means,
+                  cmap=cmocean.cm.thermal,
+                  #interpolation='bilinear',
+                  #dynamic=False,
+                  datashade=True,
+                  #x_sampling=1,#4e12,
+                  #y_sampling=0.2,
+                  cnorm='eq_hist').opts(invert_yaxis=True)#.opts(**ropts)
+
+    return points
+    """
+    return raster
+
+
+def get_xsection(x_range):
+    variable='temperature'
     (x0, x1) = x_range
     dt = x1-x0
     dtns = dt/np.timedelta64(1, 'ns')
@@ -167,12 +219,93 @@ def get_xsection(x_range, variable):
     # note: only the first plot in the list needs the **ropts. Everything else migh
     for dsid in meta.index:
         data=dsdict[dsid] if zoomed_out else dsdict[dsid.replace('nrt', 'delayed')]
-        data = data.isel(time=slice(0,-1,10), drop=True)#data.to_dask_dataframe().sample(0.1)
+        #data = data.isel(time=slice(0,-1,10), drop=True)#data.to_dask_dataframe().sample(0.1)
         #data = data.drop
         #import pdb; pdb.set_trace()
         single_plot = create_single_ds_plot(data, metadata, variable, dsid, plt_props)
         plotslist.append(single_plot)
     return reduce(lambda x, y: x*y, plotslist)
+
+
+
+def get_xsection_raster(x_range, variable):
+    (x0, x1) = x_range
+    dt = x1-x0
+    dtns = dt/np.timedelta64(1, 'ns')
+    #import pdb; pdb.set_trace()
+    #nrtkeys = [item for item in dsdict.keys() if item[0:3]=='nrt']
+    #meta = metadata.loc[nrtkeys]
+    plt_props = {}
+    #delayedkeys = [item for item in dsdict.keys() if item[0:7]=='delayed']
+    #nrtkeys =  [item for item in dsdict.keys() if item[0:3]=='nrt']
+    meta = metadata[
+            # x0 and x1 are the time start and end of our view, the other times
+            # are the start and end of the individual datasets. To increase
+            # perfomance, datasets are loaded only if visible, so if
+            # 1. it starts within our view...
+            ((pd.to_datetime(metadata['time_coverage_start (UTC)'].dt.date)>=x0) &
+            (pd.to_datetime(metadata['time_coverage_start (UTC)'].dt.date)<=x1)) |
+            # 2. it ends within our view...
+            ((pd.to_datetime(metadata['time_coverage_end (UTC)'].dt.date)>=x0) &
+            (pd.to_datetime(metadata['time_coverage_end (UTC)'].dt.date)<=x1)) |
+            # 3. it starts before and ends after our view (zoomed in)...
+            ((pd.to_datetime(metadata['time_coverage_start (UTC)'].dt.date)<=x0) &
+            (pd.to_datetime(metadata['time_coverage_end (UTC)'].dt.date)>=x1)) |
+            # 4. or it both, starts and ends within our view (zoomed out)...
+            ((pd.to_datetime(metadata['time_coverage_start (UTC)'].dt.date)>=x0) &
+            (pd.to_datetime(metadata['time_coverage_end (UTC)'].dt.date)<=x1))
+            ]
+    zoomed_out = False
+    # grid timeline into n sections
+    #plt_props['x_sampling'] = int(dtns/1000) # effective horizontal resolution (px)
+    #plt_props['y_sampling']=.2
+    #plt_props['dynfontsize']=4
+    #plt_props['x_sampling']=8.64e13/240
+
+    zoomed_in = False
+    if (x1-x0)>np.timedelta64(180, 'D'):
+        # activate sparse data mode to speed up reactivity
+        zoomed_out = True
+        #x_sampling=8.64e13 # daily
+        # grid timeline into n sections
+        plt_props['x_sampling'] = int(dtns/1000)
+        plt_props['y_sampling']=1
+        plt_props['dynfontsize']=4
+    elif (x1-x0)<np.timedelta64(1, 'D'):
+        # activate sparse data mode to speed up reactivity
+        zoomed_in = True
+        #variable='temperatu'
+        #x_sampling=8.64e13 # daily
+        # grid timeline into n sections
+        plt_props['x_sampling'] = 1#int(dtns/10000)
+        plt_props['y_sampling']=0.1
+        plt_props['dynfontsize']=4
+    else:
+        # load delayed mode datasets for more detail
+        zoomed_out = False
+        plt_props['x_sampling']=8.64e13/24
+        plt_props['y_sampling']=0.2
+        plt_props['dynfontsize']=10
+
+    plotslist = []
+    varlist = [dsdict[dsid.replace('nrt', 'delayed')][['time', 'depth', 'temperature']] for dsid in meta.index]
+    dsconc = xarray.concat(varlist, dim='time')
+    mplt = create_single_ds_plot_raster(dsconc, metadata, variable, plt_props)
+    # note: only the first plot in the list needs the **ropts. Everything else migh
+    #for dsid in meta.index:
+    #    import pdb; pdb.set_trace();
+    #    data=dsdict[dsid] if zoomed_out else dsdict[dsid.replace('nrt', 'delayed')]
+    #    data = data.isel(time=slice(0,-1,10), drop=True)#data.to_dask_dataframe().sample(0.1)
+        #data = data.drop
+        #import pdb; pdb.set_trace()
+        #single_plot = create_single_ds_plot_raster(data, metadata, variable, dsid, plt_props)
+        #plotslist.append(single_plot)
+    return mplt#reduce(lambda x, y: x*y, plotslist)
+
+
+
+
+
 
 # on initial load, show all data
 x_range=(metadata['time_coverage_start (UTC)'].min().to_datetime64(),
@@ -189,11 +322,15 @@ def create_dynmap(icnorm):
     global cnorm
     cnorm = variable_widget.value
     dmap = hv.DynamicMap(get_xsection,
+    #kdims=['variable'],#, 'cnorm'],
+    streams=[range_stream],)
+    dmap_raster = hv.DynamicMap(get_xsection_raster,
     kdims=['variable'],#, 'cnorm'],
     streams=[range_stream],)
-    return dmap.redim.values(
-        variable=('temperature', 'salinity'))
+    return dmap, dmap_raster#.redim.values(
+        #variable=('temperature', 'salinity'))
 
+"""
 plotslist2 = []
 for dsid in metadata.index:
     data=dsdict[dsid.replace('nrt', 'delayed')]
@@ -223,13 +360,31 @@ for dsid in metadata.index:
         )
     plotslist2.append(raster)
 plotslist2 = reduce(lambda x, y: x*y, plotslist2)
+"""
+
 
 # one solution to the reset problem could be to change global values, e.g. set variable to "selected temperature" globally
-dmap = pn.bind(
-        create_dynmap,
-        icnorm=variable_widget)
+#dmap = pn.bind(
+#        create_dynmap,
+#        icnorm=variable_widget)
 
-pn.Column(plotslist2*create_dynmap('linear')).show(port=12345)
+#import pdb; pdb.set_trace()
+#pn.Column(plotslist2*create_dynmap('linear')).show(port=12345)
+dmap, dmap_raster = create_dynmap('linear')
+dmap_raster = dmap_raster.redim.values(
+        variable=('temperature', 'salinity'))
+means = dsh.mean('temperature')
+#pn.Column((rasterize(dmap_raster, aggregator=means)*dmap).opts(**ropts)).show(port=12345)
+pn.Column(rasterize(dmap_raster, aggregator=means).opts(
+             invert_yaxis=True,
+             colorbar=True,
+             cmap=cmocean.cm.thermal,
+             toolbar='above', tools=['xwheel_zoom', 'reset', 'xpan', 'ywheel_zoom', 'ypan'],
+             default_tools=[],
+             responsive=True,
+             height=400,
+             active_tools=['xpan', 'xwheel_zoom'],
+             bgcolor="dimgrey",).opts(**ropts)).show(port=12345)
 #pn.Column(variable_widget,dmap).show(port=12345)
 
 """
